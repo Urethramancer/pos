e1ce5e8121aa9a7c4280eaaa6e70fefbbb006dfb
Ronny Bangsund <github@neuralshortcircuit.com>
2019-08-18 15:21:27 +0200

    Implemented the rest of the contact shell functionality.

--- internal/shell/sh_contact.go
+++ internal/shell/sh_contact.go
@@ -24,14 +24,54 @@ func (sh *Shell) contactCommands(args []string) {
 	cmd := args[0]
 	args = args[1:]
 	switch cmd {
-	case "list":
 	case "add":
 		sh.addContact()
 
 	case "edit":
+		if len(args) == 0 {
+			sh.m("You must specify a contact ID to edit.")
+			return
+		}
+
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			sh.e("Error parsing ID: %s", err.Error())
+			break
+		}
+
+		sh.editContact(int64(id))
+
 	case "remove":
+		if len(args) == 0 {
+			sh.m("You must specify one or more contact IDs to remove.")
+			return
+		}
+
+		for _, x := range args {
+			id, err := strconv.Atoi(x)
+			if err != nil {
+				sh.e("Error parsing ID: %s", err.Error())
+				break
+			}
+			sh.removeContact(int64(id))
+		}
+
+	case "list":
+		sh.listContacts()
+
 	case "show":
+		if len(args) == 0 {
+			sh.m("You must specify one or more contact IDs to show.")
+			return
+		}
+		sh.showContacts(args)
+
 	case "find":
+		if len(args) == 0 {
+			sh.m("You must specify a keyword to search for.")
+			return
+		}
+		sh.findContacts(args[0])
 	}
 }
 
@@ -48,6 +88,27 @@ func (sh *Shell) addContact() {
 	}
 
 	sh.m("Added client %s with ID %d.", c.Name, id)
+}
+
+func (sh *Shell) editContact(id int64) {
+	c := sh.db.GetContact(id)
+	if c == nil {
+		sh.e("No client with that ID.")
+		return
+	}
+
+	c = sh.promptContact(c)
+	if c == nil {
+		return
+	}
+
+	err := sh.db.UpdateContact(c)
+	if err != nil {
+		sh.e("Error updating client: %s", err.Error())
+		return
+	}
+
+	sh.m("Updated client %s with ID %d.", c.Name, c.ID)
 }
 
 func (sh *Shell) promptContact(c *database.Contact) *database.Contact {
@@ -132,23 +193,14 @@ func (sh *Shell) promptContact(c *database.Contact) *database.Contact {
 	return c
 }
 
-func printContacts(list []*database.Contact) {
-	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
-	s := stringer.New()
-	s.WriteString("ID\tClient\tE-mail\tPhone\tAddress\tVAT ID\tCreated\n")
-	for _, c := range list {
-		s.WriteI(
-			c.ID,
-			"\t", c.Name,
-			"\t", c.Email,
-			"\t", c.Phone,
-			"\t", c.Client,
-			"\t", c.Created.String(),
-			"\n",
-		)
+func (sh *Shell) removeContact(id int64) {
+	err := sh.db.RemoveContact(id)
+	if err != nil {
+		sh.e("Error removing contact: %s", err.Error())
+		return
 	}
-	_, _ = tw.Write([]byte(s.String()))
-	tw.Flush()
+
+	sh.m("Removed contact %d.", id)
 }
 
 func (sh *Shell) listContacts() {
@@ -158,5 +210,70 @@ func (sh *Shell) listContacts() {
 		return
 	}
 
-	printContacts(list)
+	sh.printContacts(list)
+}
+
+func (sh *Shell) showContacts(idlist []string) {
+	var list []*database.Contact
+	for _, id := range idlist {
+		x, err := strconv.Atoi(id)
+		if err != nil {
+			return
+		}
+		c := sh.db.GetContact(int64(x))
+		if c == nil {
+			sh.m("No contact with ID %s.", id)
+		} else {
+			list = append(list, c)
+		}
+	}
+	if len(list) == 0 {
+		sh.m("No clients found with supplied ID(s).")
+	} else {
+		sh.printContacts(list)
+	}
+}
+
+func (sh *Shell) findContacts(keyword string) {
+	list, err := sh.db.GetContacts(keyword)
+	if err != nil {
+		sh.e("Error retrieving clients: %s", err.Error())
+		return
+	}
+
+	if len(list) == 0 {
+		sh.m("No clients found.")
+		return
+	}
+
+	sh.printContacts(list)
+}
+
+func (sh *Shell) printContacts(list []*database.Contact) {
+	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
+	s := stringer.New()
+	s.WriteString("ID\tClient\tE-mail\tPhone\tClient\tCreated\n")
+	for _, c := range list {
+		company := "Unknown"
+		client := sh.db.GetClient(c.Client)
+		if client != nil {
+			company = client.Company
+		}
+		_, err := s.WriteI(
+			c.ID,
+			"\t", c.Name,
+			"\t", c.Email,
+			"\t", c.Phone,
+			"\t", c.Client, " (", company, ")",
+			"\t", c.Created.String(),
+			"\n",
+		)
+
+		if err != nil {
+			sh.e("Error printing to stdout: %s", err.Error())
+			return
+		}
+	}
+	_, _ = tw.Write([]byte(s.String()))
+	tw.Flush()
 }

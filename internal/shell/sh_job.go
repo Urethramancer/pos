c443bf958e9e24bc09c73c71ec0e2ce82b8877d6
Ronny Bangsund <github@neuralshortcircuit.com>
2019-08-18 20:56:57 +0200

    Edit, remove and list (all and specific client) implemented.

--- internal/shell/sh_job.go
+++ internal/shell/sh_job.go
@@ -1,17 +1,20 @@
 package shell
 
 import (
+	"os"
 	"strconv"
+	"text/tabwriter"
 
 	"github.com/Urethramancer/pos/internal/database"
+	"github.com/Urethramancer/signor/stringer"
 )
 
 func (sh *Shell) jobCommands(args []string) {
 	if len(args) == 0 {
 		sh.m("list\t\tList all jobs, optionally only for a client ID.")
 		sh.m("add\t\tAdd job. Takes client ID and currency code as arguments.")
-		sh.m("edit <id>\t\tModify a job. Same arguments as adding.")
-		sh.m("remove <id>\tRemove a job by ID. Will also remove all related tasks.")
+		sh.m("edit <id>\tModify a job. Same arguments as adding.")
+		sh.m("remove <id...>\tRemove jobs by ID. Will also remove all related tasks.")
 		sh.m("show <id>\tShow details and tasks for a job.")
 		return
 	}
@@ -25,9 +28,42 @@ func (sh *Shell) jobCommands(args []string) {
 		} else {
 			sh.addJob(args)
 		}
+
 	case "edit":
+		if len(args) < 3 {
+			sh.m("You need to specify a job ID, client ID and currency code. Example:\n\tjob edit 1 1 GBP")
+		} else {
+			sh.editJob(args)
+		}
+
 	case "remove":
+		if len(args) == 0 {
+			sh.m("You must specify one or more job IDs to remove.")
+			return
+		}
+
+		for _, x := range args {
+			id, err := strconv.Atoi(x)
+			if err != nil {
+				sh.e("Error parsing ID: %s", err.Error())
+				break
+			}
+			sh.removeJob(int64(id))
+		}
+
 	case "list":
+		if len(args) < 1 {
+			sh.listJobs()
+		} else {
+			id, err := strconv.Atoi(args[0])
+			if err != nil {
+				sh.e("Error converting ID.")
+				break
+			}
+
+			sh.listJobsFor(int64(id))
+		}
+
 	case "show":
 	}
 }
@@ -45,9 +81,95 @@ func (sh *Shell) addJob(args []string) {
 	}
 	id, err := sh.db.AddJob(j)
 	if err != nil {
-		sh.e("Error adding client: %s", err.Error())
+		sh.e("Error adding job: %s", err.Error())
 		return
 	}
 
 	sh.m("Added job with ID %d to client %d.", id, client)
+}
+
+func (sh *Shell) editJob(args []string) {
+	var err error
+	id, err := strconv.Atoi(args[0])
+	if err != nil {
+		sh.e("Error converting ID: %s", err.Error())
+		return
+	}
+
+	client, err := strconv.Atoi(args[1])
+	if err != nil {
+		sh.e("Error converting ID: %s", err.Error())
+		return
+	}
+
+	j := &database.Job{
+		ID:       int64(id),
+		Client:   int64(client),
+		Currency: args[1],
+	}
+	err = sh.db.UpdateJob(j)
+	if err != nil {
+		sh.e("Error editing job: %s", err.Error())
+		return
+	}
+
+	sh.m("Updated job %d for client %d.", id, client)
+}
+
+func (sh *Shell) removeJob(id int64) {
+	err := sh.db.RemoveJob(id)
+	if err != nil {
+		sh.e("Error removing job: %s", err.Error())
+		return
+	}
+
+	sh.m("Removed job %d.", id)
+}
+
+func (sh *Shell) listJobs() {
+	list, err := sh.db.GetAllJobs()
+	if err != nil {
+		sh.e("Error retrieving job list: %s", err.Error())
+		return
+	}
+
+	sh.printJobs(list)
+}
+
+func (sh *Shell) listJobsFor(id int64) {
+	list, err := sh.db.GetJobsFor(id)
+	if err != nil {
+		sh.e("Error retrieving job list: %s", err.Error())
+		return
+	}
+
+	sh.printJobs(list)
+}
+
+func (sh *Shell) printJobs(list []*database.Job) {
+	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 2, '\t', 0)
+	s := stringer.New()
+	s.WriteString("ID\tClient\tCurrency\tCost\tCreated\n")
+	for _, j := range list {
+		company := "Unknown"
+		client := sh.db.GetClient(j.Client)
+		if client != nil {
+			company = client.Company
+		}
+		_, err := s.WriteI(
+			j.ID,
+			"\t", j.Client, " (", company, ")",
+			"\t", j.Currency,
+			"\t", j.Cost,
+			"\t", j.Created.String(),
+			"\n",
+		)
+
+		if err != nil {
+			sh.e("Error printing to stdout: %s", err.Error())
+			return
+		}
+	}
+	_, _ = tw.Write([]byte(s.String()))
+	tw.Flush()
 }
